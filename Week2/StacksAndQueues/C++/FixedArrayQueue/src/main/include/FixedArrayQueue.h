7dde09f5b0f2db5ddeec8a5a87c344279bbd3c89
Daniel Cumberbatch <daniel.cumberbatch1@gmail.com>
2018-10-22 01:46:03 +0100

    Week2: Added move structors to FixedArrayQueue; Added copying tests.

--- Week2/StacksAndQueues/C++/FixedArrayQueue/src/main/include/FixedArrayQueue.h
+++ Week2/StacksAndQueues/C++/FixedArrayQueue/src/main/include/FixedArrayQueue.h
@@ -18,6 +18,8 @@ namespace queues
     explicit FixedArrayQueue(std::size_t N);
     FixedArrayQueue(const FixedArrayQueue& other);
     FixedArrayQueue& operator=(const FixedArrayQueue& other);
+    FixedArrayQueue(FixedArrayQueue&&) = default;
+    FixedArrayQueue& operator=(FixedArrayQueue&&) = default;    
     ~FixedArrayQueue();
 
     bool empty() const;
--- Week2/StacksAndQueues/C++/FixedArrayQueue/src/test/C++/FixedArrayQueueTests.cpp
+++ Week2/StacksAndQueues/C++/FixedArrayQueue/src/test/C++/FixedArrayQueueTests.cpp
@@ -111,5 +111,77 @@ namespace queues
       ASSERT_EQ(q.capacity(), 0);
     }
 
+    TEST(FixedArrayQueue, CopyConstrutor)
+    {
+      FixedArrayQueue<int> q1(10);
+
+      for (int i = 0; i < 10; ++i)
+      {
+	q1.enqueue(i);
+      }
+
+      FixedArrayQueue<int> q2(q1);
+
+      while (!q1.empty())
+      {
+	ASSERT_EQ(q1.size(), q1.capacity()); 
+	ASSERT_EQ(q1.front(), 10-q1.size());
+	ASSERT_EQ(q1.back(), 9);
+	q1.dequeue();
+      }
+
+      ASSERT_TRUE(q1.empty());
+      ASSERT_EQ(q1.size(), 0);
+
+      ASSERT_FALSE(q2.empty());
+      ASSERT_EQ(q2.size(), 10);
+
+      while (!q2.empty())
+      {
+	ASSERT_EQ(q2.front(), 10-q2.size());
+	ASSERT_EQ(q2.back(), 9);
+	q2.dequeue();
+      }
+
+      ASSERT_TRUE(q2.empty());
+      ASSERT_EQ(q2.size(), 0);
+    }
+
+    TEST(FixedArrayQueue, CopyAssignment)
+    {
+      FixedArrayQueue<int> q1(10);
+
+      for (int i = 0; i < 10; ++i)
+      {
+	q1.enqueue(i);
+      }
+
+      FixedArrayQueue<int> q2 = q1;
+
+      while (!q1.empty())
+      {
+	ASSERT_EQ(q1.size(), q1.capacity()); 
+	ASSERT_EQ(q1.front(), 10-q1.size());
+	ASSERT_EQ(q1.back(), 9);
+	q1.dequeue();
+      }
+
+      ASSERT_TRUE(q1.empty());
+      ASSERT_EQ(q1.size(), 0);
+
+      ASSERT_FALSE(q2.empty());
+      ASSERT_EQ(q2.size(), 10);
+
+      while (!q2.empty())
+      {
+	ASSERT_EQ(q2.front(), 10-q2.size());
+	ASSERT_EQ(q2.back(), 9);
+	q2.dequeue();
+      }
+
+      ASSERT_TRUE(q2.empty());
+      ASSERT_EQ(q2.size(), 0);
+    }
+
   } // ! namespace queuetesting
 } // ! namespace queues
