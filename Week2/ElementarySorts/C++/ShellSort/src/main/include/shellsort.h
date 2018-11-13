aca42ca76d2448f854a995a1fc1e957732aed73d
Daniel Cumberbatch <daniel.cumberbatch1@gmail.com>
2018-11-13 09:38:26 +0000

    Week2: Implemented Shellsort in C++

---
+++ Week2/ElementarySorts/C++/ShellSort/src/main/include/shellsort.h
@@ -0,0 +1,14 @@
+#ifndef SRC_MAIN_INCLUDE_SHELLSORT_H_
+#define SRC_MAIN_INCLUDE_SHELLSORT_H_
+
+namespace shellsort
+{
+  template<typename T>
+  void sort(T a[], std::size_t N)
+  {
+    
+  }
+
+} // ! namespace shellsort
+
+#endif // ! SRC_MAIN_INCLUDE_SHELLSORT_H_
