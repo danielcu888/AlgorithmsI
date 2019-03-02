11bb1be0414373809020eba2da23fe6374d514bf
Daniel Cumberbatch <daniel.cumberbatch1@gmail.com>
2019-03-02 04:44:43 +0000

    Week1: Java: Implemented stubs for Percolation.

---
+++ Week1/Assignment/Java/Percolation/src/Percolation.java
@@ -0,0 +1,63 @@
+
+public class Percolation {
+	
+	/**
+	 * Create an n-by-n grid, with all sites blocked.
+	 * @param n The dimension of the grid to be
+	 *        constructed.
+	 */
+	public Percolation(int n) {
+		// TODO - implement
+		throw new UnsupportedOperationException("Percolation.Percolation(int) is unimplemented.");
+	}
+	   
+	/**
+	 * Open site (row, col) if it is not open already.
+	 * @param row The row index of the site to be opened (>= 0).
+	 * @param col The column index of the site to be opened (>= 0).
+	 */
+	public void open(int row, int col) {
+		// TODO - implement
+		throw new UnsupportedOperationException("Percolation.open(int,int) is unimplemented.");
+	}
+	
+	/**
+	 * Return if the specified site is open.
+	 * @param row The row index of the specified site (>= 0).
+	 * @param col The column index of the specified site (>= 0).
+	 * @return If the specified site is open.
+	 */
+	public boolean isOpen(int row, int col) {
+		// TODO - implement
+		throw new UnsupportedOperationException("Percolation.isOpen(int,int) is unimplemented.");
+	}
+
+	/**
+	 * Return if the specified site is full.
+	 * @param row The row index of the specified site (>= 0).
+	 * @param col The column index of the specified site (>= 0).
+	 * @return If the specified site is full.
+	 */
+	public boolean isFull(int row, int col) {
+		// TODO - implement
+		throw new UnsupportedOperationException("Percolation.isFull(int,int) is unimplemented.");		
+	}
+	
+	/**
+	 * Return the number of open sites.
+	 * @return The number of open sites.
+	 */
+	public int numberOfOpenSites() {
+		// TODO - implement
+		throw new UnsupportedOperationException("Percolation.numberOfOpenSites() is unimplemented.");				
+	}
+	
+	/**
+	 * Return if the system percolates.
+	 * @return If the system percolates.
+	 */
+	public boolean percolates() {
+		// TODO - implement
+		throw new UnsupportedOperationException("Percolation.percolates() is unimplemented.");						
+	}
+}
