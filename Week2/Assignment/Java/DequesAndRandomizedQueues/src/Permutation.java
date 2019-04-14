39f08af62d99067dbfb4be29ce582884bf5bd5ad
Daniel Cumberbatch <daniel.cumberbatch1@gmail.com>
2019-04-14 18:40:43 +0100

    Week2: Java: Assignment: Fixed typo in Permutation.java.

--- Week2/Assignment/Java/DequesAndRandomizedQueues/src/Permutation.java
+++ Week2/Assignment/Java/DequesAndRandomizedQueues/src/Permutation.java
@@ -3,7 +3,7 @@ import edu.princeton.cs.algs4.StdIn;
 public class Permutation {
 	public static void main(String[] args) {
 		
-		final int k = StdIn.readInt();
+		final int k = Integer.parseInt(args[0]);
 
 		final RandomizedQueue<String> rq = new RandomizedQueue<String>();
 		while (!StdIn.isEmpty()) {
